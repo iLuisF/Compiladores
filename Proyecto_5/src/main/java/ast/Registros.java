2e5f9a034f974a8cfa99f277a6cf5646bd71e0fb
Luis Flores González <iluis@ciencias.unam.mx>
2018-06-01 07:01:06 -0500

    Registros para generar código.

---
+++ Proyecto_5/src/main/java/ast/Registros.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ast;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Diana Montes.
+ * 
+ */
+public class Registros{
+
+  int objetivoEntero;
+  int objetivoFlotante;
+    // Todos los registros enteros disponibles
+  String[] E_registros;
+    // Todos los registros flotantes disponibles
+  String[] F_registros;
+
+    public Registros() {
+        this.F_registros = new String[]{"$f4","$f5","$f6","$f7","$f8","$f9","$f10"};
+        this.E_registros = new String[]{"$t0","$t1","$t2","$t3","$t4","$t5","$t6","$t7","$t8","$t9"};
+    }
+
+  public void setObjetivo(int o, boolean entero){
+      if(entero){
+          objetivoEntero = o % E_registros.length;
+      }else{
+          objetivoFlotante = o % F_registros.length;
+      }
+
+  }
+
+  public void setObjetivo(String o, boolean entero){
+      String[] registros;
+
+      if(entero){
+         registros = E_registros;
+      }else{
+         registros = F_registros;
+      }
+      int nvo_objetivo = Arrays.asList(registros).indexOf(o);
+      setObjetivo(nvo_objetivo, entero);
+  }
+
+  public int getObjetivo(boolean entero){
+      if(entero){
+          return objetivoEntero;
+      }else{
+          return objetivoFlotante;
+      }
+  }
+
+  /* Regresa los n registos siguientes "disponibles" */
+  public String[] getNsiguientes(int n, boolean entero){
+      String[] siguientes = new String[n];
+      int objetivo;
+      String[] registros;
+
+      if(entero){
+          objetivo = objetivoEntero;
+          registros = E_registros;
+      }else{
+          objetivo = objetivoFlotante;
+          registros = F_registros;
+      }
+
+      for(int i = 0; i < n; i++){
+          siguientes[i] = registros[(objetivo + i) % E_registros.length];
+      }
+      return siguientes;
+  }
+}
